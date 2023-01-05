f1606ffe38bd0ec060be2ed928115cdfcdf80068
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-01-05 17:32:29 +0100

    App.js - createnavigatorstack probleempje opgelost bij een error die ik kreeg

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+/*import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View , FlatList, Pressable} from 'react-native';
+import React, {useState, useEffect} from 'react';
 
+
+export default function App() {
+  const [plants, setPlants] = useState ([]);
+
+    const getPlants = async () => {
+        try {
+            const response = await fetch ("http://jarobrichau.be/wp-json/wp/v2/posts?categories=6", {
+
+            })
+            const json = await response.json();
+            console.log(json)
+            setPlants(json);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getPlants();
+    }, []);
+  return (
+    <View>
+    <FlatList data={plants} renderItem={({item})=>(
+        <Text>
+            <Text>{item.title.rendered}</Text>
+            <Pressable>Verder</Pressable>
+        </Text>
+      )}
+
+ />
+</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import stofzuigers from './screens/stofzuigers.js';
+import meubels from './screens/AllStofzuigerSreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -14,7 +60,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Allerlei stofzuigers" component={(stofzuigers)} />
+        <Stack.Screen name="Trendy meubels" component={(meubels)} />
       </Stack.Navigator>
     </NavigationContainer>
   );
