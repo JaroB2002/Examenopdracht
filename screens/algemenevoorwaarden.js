fc87c039b9e540d1292c8fbd686763a8f914280c
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-01-15 10:15:01 +0100

    Create algemenevoorwaarden.js
    Added algemene voorwaarden screen

---
+++ screens/algemenevoorwaarden.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList, Vibration} from 'react-native';
+
+const AlgemeneVoorwaarden = ({navigation}) =>{
+
+    return (
+        <View style={Styles.screen}>
+            <Text>
+                Terms and Conditions for [D3JB] 
+                Introduction
+                This document outlines the terms and conditions (the "Terms") under which you, the user, may use the [D3JB] application (the "App") and any associated services, content, or features (collectively, the "Services"). By accessing or using the Services, you agree to be bound by these Terms. If you do not agree to these Terms, you may not use the Services.
+                Use of the Services
+                You may use the Services only if you are 18 years of age or older and have the legal capacity to enter into a binding contract. By using the Services, you represent and warrant that you meet these requirements.
+                Proprietary Rights
+                The Services and all associated content, including but not limited to text, images, graphics, logos, and software, are the property of [D3JB] or its licensors and are protected by copyright and trademark laws. You may not use any such content without the express written consent of [D3JB].
+
+                User Content
+                The Services may allow you to upload, submit, or otherwise make available content, including but not limited to text, images, and videos (collectively, "User Content"). You retain all rights in and to your User Content, but by making it available through the Services, you grant [D3JB] a worldwide, non-exclusive, royalty-free, sublicensable, and transferable license to use, reproduce, distribute, prepare derivative works of, display, and perform your User Content in connection with the Services and [D3B]'s business, including without limitation for promoting and redistributing part or all of the Services in any media formats and through any media channels.
+
+                Disclaimer of Warranties
+                THE SERVICES ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. [D3JB] MAKES NO WARRANTY THAT THE SERVICES WILL MEET YOUR REQUIREMENTS, OR THAT THE SERVICES WILL BE UNINTERRUPTED, TIMELY, SECURE, OR ERROR-FREE.
+
+                Limitation of Liability
+                IN NO EVENT SHALL [D3JB] BE LIABLE FOR ANY DAMAGES (INCLUDING, WITHOUT LIMITATION, INCIDENTAL AND CONSEQUENTIAL DAMAGES, LOST PROFITS, OR DAMAGES RESULTING FROM LOST DATA OR BUSINESS INTERRUPTION) RESULTING FROM THE USE OR INABILITY TO USE THE SERVICES, OR FROM ANY CONTENT OBTAINED FROM OR THROUGH THE SERVICES, WHETHER BASED ON WARRANTY, CONTRACT, TORT, OR ANY OTHER LEGAL THEORY, AND WHETHER OR NOT [D3JB] IS ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+
+                Indemnification
+                You agree to indemnify and hold [D3JB], its affiliates, officers, agents, and employees harmless from any claim or demand, including reasonable attorneys' fees, made by any third party due to or arising out of your use of the Services, your violation of these Terms, or your violation of any rights of another.
+
+                General Provisions
+                These Terms constitute the entire agreement between you and [D3JB] regarding the use of the Services and supersede all prior agreements and understandings, whether written or oral. If any provision of these Terms is held to be
+
+            </Text>
+        </View>
+    )
+}
