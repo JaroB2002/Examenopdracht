1d48ec9f3ee6ae7818725707fb98eb76237c8fd2
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-01-15 19:49:56 +0100

    Update App.js
    Code cleaning

--- App.js
+++ App.js
@@ -2,9 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableNativeFeedback, ToastAndroid  } from 'react-native';
-
-//import { WebViewLeaf } from 'react-native-webview-leaf';
+import { StyleSheet, Text, View  } from 'react-native';
 
 import Stofzuigers from './screens/stofzuigers.js';
 import Info from './screens/info.js';
@@ -12,9 +10,7 @@ import Winkelcartscreen from './screens/winkelcart.js';
 import AlgemeneVoorwaarden from './screens/algemenevoorwaarden.js';
 
 const Stack = createNativeStackNavigator();
-// function showToast() {
-//   ToastAndroid.show('Request sent successfully!', ToastAndroid.SHORT);
-// }
+
 export default function App() {
   return (
     <NavigationContainer>
@@ -27,26 +23,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  /*container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: StatusBar.currentHeight,
-    backgroundColor: '#6638f0',
-    padding: 8,
-  },*/
-
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-    fontFamily: "SFUIDisplay-Bold"
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-    fontFamily: "SFUIDisplay-Semibold"
-  }
-});
